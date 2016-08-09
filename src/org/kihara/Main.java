c30fe1de090232d2b1f82e3b8a9a34caac0f339f
Andrew Walden <waldena@miffy.bio.purdue.edu>
2016-08-09 11:58:39 -0400

    Added .so file, test code

--- src/org/kihara/Main.java
+++ src/org/kihara/Main.java
@@ -27,6 +27,7 @@ import com.avaidyam.binoculars.util.Log;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
 import org.kihara.util.ParameterFilter;
+import org.kihara.lzerd.LZerDWrapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -49,6 +50,11 @@ public class Main {
      * Initialize the Cortex, and start the HTTP server and shell.
      */
     public static void main(String[] args) {
+	LZerDWrapper.runLZerD("rec", "lig", "rzec", "zlig",
+                                "prec", "plig", "irec", "ilig",
+                                13, 14, 15, 16,
+                                17, 18, 19, false,
+                                "output_filename");
         Log.get().setSeverity(Log.Severity.DEBUG);
         Cortex<PFPController> cortex = Cortex.of(PFPController.class);
         try {
