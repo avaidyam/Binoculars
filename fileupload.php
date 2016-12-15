070c2ede985bb398f3f10f907c9a60a17e49e303
Andrew Walden <waldena@dragon.bio.purdue.edu>
2016-12-15 10:47:02 -0500

    File upload now supported on dragon

--- fileupload.php
+++ fileupload.php
@@ -4,18 +4,28 @@
 	} else {
 
 		$upload_folder = "upload/";
-		echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-		echo "Type: " . $_FILES["file"]["type"] . "<br>";
-		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-		echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+		$upload_folder = getcwd() . "/" . $upload_folder;
+		foreach ($_FILES as $file) {
+			echo "Upload: " . $file["name"] . "<br>";
+			echo "Type: " . $file["type"] . "<br>";
+			echo "Size: " . ($file["size"] / 1024) . " kB<br>";
+			echo "Temp file: " . $file["tmp_name"] . "<br>";
 
-		if (file_exists($upload_folder . $_FILES["file"]["name"])) {
-			echo $_FILES["file"]["name"] . " already exists. ";
-		} else {
-			mkdir($upload_folder.$title, 0700);
-			move_uploaded_file($_FILES["file"]["tmp_name"],
-			$upload_folder.$title . $_FILES["file"]["name"]);
-			echo "Stored in: " .$upload_folder.$title . $_FILES["file"]["name"];
+			if (file_exists($upload_folder . $file["name"])) {
+				echo $file["name"] . " already exists. ";
+			} else {
+				$try_mkdir = mkdir($upload_folder.$title, 0777);
+				if ($try_mkdir) {
+					echo "MKDIR Success!<br>";
+				}
+				else {
+					echo "MKDIR Failure!<br>";
+				}
+				move_uploaded_file($file["tmp_name"],
+				$upload_folder.$title . $file["name"]);
+				echo "Stored in: " .$upload_folder.$title . $file["name"];
+			}
+			echo "<br>";
 		}
 	}
 ?>
